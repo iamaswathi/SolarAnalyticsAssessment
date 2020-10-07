6aaf1c1f9f6330ea191e6303dfa0fcfea4a5a263
Aswathi Prakash <inboxaswathi@gmail.com>
2020-10-07 21:27:09 +1100

    removed commented code from the files

--- src/app/common/theme/dark-theme.ts
+++ src/app/common/theme/dark-theme.ts
@@ -3,10 +3,6 @@ import { Theme } from './theme';
 export const darkTheme: Theme = {
   name: 'darkTheme',
   properties: {
-    '--background': '#1F2125',
-    '--on-background': '#fff',
-    '--primary': 'darkorange',
-    '--on-primary': '#fff',
     '--font-color': 'hsl(0, 0%, 100%)',
     '--bg-color': 'hsl(207, 26%, 17%)',
     '--input-color': 'hsl(0, 0%, 52%)',
